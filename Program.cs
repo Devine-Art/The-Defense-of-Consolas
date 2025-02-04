acc9dc62a1b2ea59cbbab81830e5d5c680e9b762
Devine-Art <adevine149@gmail.com>
2025-02-04 17:18:54 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Dominion_of_Kings
+{
+    public class Program
+{
+    static void Main(string[] args)
+    { 
+        //Change the window title
+        Console.Title = "Defense of Consolas";
+
+        //ask user for variables and assign them
+        Console.WriteLine("Enter target row");
+        int row = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter target column");
+        int column = int.Parse(Console.ReadLine());
+
+
+
+        //Calculate squad positions
+        int topMan = row + 1;
+        int eastMan = column + 1;
+        int bottomMan = row - 1;
+        int westMan = column - 1;
+
+        //Convert variables to strings to be used in writing results to console
+        string topManpos = topMan.ToString();
+        string eastManPos = eastMan.ToString();
+        string bottomManpos = bottomMan.ToString();
+        string westManpos = westMan.ToString();
+
+
+        //change color of text
+        Console.ForegroundColor = ConsoleColor.Blue;
+
+        //write results to console
+        Console.WriteLine("deploy squad to:");
+        Console.WriteLine(topManpos, column);
+        Console.WriteLine(row, eastManPos);
+        Console.WriteLine(bottomManpos, column);
+        Console.WriteLine(row, westManpos);
+        //this works because you only need to change 1 variable to move 1 square away from the protected square
+        //make a beep
+        Console.Beep();
+
+    }
+}
+}
